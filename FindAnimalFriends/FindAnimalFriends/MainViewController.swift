4e7f3e7679cd30ea4bc77138da997a0fb34d3111
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-07-22 21:51:33 +0900

    [Feat] #2 Add background image in MainVC

--- FindAnimalFriends/FindAnimalFriends/MainViewController.swift
+++ FindAnimalFriends/FindAnimalFriends/MainViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
+extension CGFloat {
+    static let screenW = UIScreen.main.bounds.width
+    static let screenH = UIScreen.main.bounds.height
+}
+
 class MainViewController: UIViewController {
+    
+    private let backImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "mainBackground")
+        imageView.contentMode = .scaleToFill
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        
+        addSubviewsWithFrame()
     }
 }
 
+private extension MainViewController {
+    func addSubviewsWithFrame() {
+        view.addSubview(backImageView)
+        backImageView.frame = CGRect(x: 0, y: 0, width: .screenW, height: .screenH)
+    }
+}
